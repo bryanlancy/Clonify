276d5c04d4cb92063a95639c89a7e63ccb443c57
Bryan Burns <bryanburns93@gmail.com>
2021-05-13 08:39:33 -0400

    add links

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -6,7 +6,7 @@ export default function Footer() {
 		<div className="footer">
 			<div className="footer__title">
 				<i class="fab fa-spotify"></i>
-				<h2>Clonify</h2>
+				<h3>Clonify</h3>
 				<p>
 					a{' '}
 					<a href="https://www.spotify.com/" target="_blank">
@@ -17,8 +17,48 @@ export default function Footer() {
 			</div>
 			<div className="footer__useful-links">
 				<p>Useful Links</p>
-				<a>LinkedIn</a>
-				<a>Github</a>
+
+				<div>
+					<a href="https://github.com/bryanlancy/Clonify" target="_blank">
+						Github
+					</a>
+				</div>
+
+				<div>
+					<a href="">My portfolio - NEED TO ADD</a>
+				</div>
+			</div>
+			<div className="footer__other-projects">
+				<p>My Other Projects</p>
+				<div className="project-link">
+					<a href="https://merrymenstocks.herokuapp.com/" target="_blank">
+						MerryMen
+					</a>
+					<p>a Robinhood clone</p>
+				</div>
+				<div className="project-link">
+					<a href="https://www.fetchigram.com/" target="_blank">
+						Fetch
+					</a>
+					<p>an Instagram clone</p>
+				</div>
+				<div className="project-link">
+					<a href="https://queit.herokuapp.com/" target="_blank">
+						QueIt
+					</a>
+					<p>a Quora clone</p>
+				</div>
+			</div>
+			<div className="footer__connect">
+				<p>Connect With Me</p>
+				<div>
+					<a href="https://www.linkedin.com/in/bryan-burns-b45006116/" target="_blank">
+						LinkedIn
+					</a>
+				</div>
+				<div>
+					<a href="">AngelList - NEED TO ADD</a>
+				</div>
 			</div>
 		</div>
 	)
--- frontend/src/components/NavigationSimple/index.js
+++ frontend/src/components/NavigationSimple/index.js
@@ -4,7 +4,10 @@ import './NavigationSimple.css'
 export default function NavigationSimple() {
 	return (
 		<div className="navigation-simple">
-			<h2>Navigation Simple</h2>
+			<div className="nav__title">
+				<i class="fab fa-spotify"></i>
+				<h3>Clonify</h3>
+			</div>
 			<i className="fal fa-bars"></i>
 		</div>
 	)
